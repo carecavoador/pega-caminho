5586670f1624b5ab171ab7a71d151022746c2fce
Everton de Souza <e.rodrigo@outlook.com>
2023-06-28 15:51:09 -0300

    Bugfix

--- pega-caminho.py
+++ pega-caminho.py
@@ -1,17 +1,7 @@
 import sys
 from pathlib import Path
 
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QWidget,
-    QVBoxLayout,
-    QLabel,
-    QLineEdit,
-    QPushButton,
-)
-from PySide6.QtCore import QMimeData
-from PySide6.QtGui import QClipboard
+from PySide6 import QtWidgets, QtCore, QtGui
 
 from gui.droparea import DropArea
 
@@ -19,16 +9,16 @@ from gui.droparea import DropArea
 VERSAO = "0.2.1"
 
 
-class JanelaPrincipal(QMainWindow):
+class JanelaPrincipal(QtWidgets.QMainWindow):
     """Janela principal do programa."""
 
-    def __init__(self, local: str, clipboard: QClipboard) -> None:
+    def __init__(self, local: str, clipboard: QtGui.QClipboard) -> None:
         super().__init__()
         self.setWindowTitle(f"Pega Caminho v{VERSAO}")
         self.clipboard = clipboard
 
-        self.widget_central = QWidget()
-        self.layout = QVBoxLayout(self.widget_central)
+        self.widget_central = QtWidgets.QWidget()
+        self.layout = QtWidgets.QVBoxLayout(self.widget_central)
 
         # Drop area
         self.drop_area = DropArea()
@@ -36,27 +26,28 @@ class JanelaPrincipal(QMainWindow):
         self.layout.addWidget(self.drop_area)
 
         # Caminho original
-        self.label_original = QLabel("Caminho original:")
+        self.label_original = QtWidgets.QLabel("Caminho original:")
         self.layout.addWidget(self.label_original)
 
-        self.edit_original = QLineEdit()
+        self.edit_original = QtWidgets.QLineEdit()
         self.edit_original.setPlaceholderText("Cole aqui o caminho original...")
         self.layout.addWidget(self.edit_original)
 
         # Caminho novo
-        self.label_novo = QLabel("Novo caminho:")
+        self.label_novo = QtWidgets.QLabel("Novo caminho:")
         self.layout.addWidget(self.label_novo)
-        self.edit_novo = QLineEdit()
+        self.edit_novo = QtWidgets.QLineEdit()
         self.layout.addWidget(self.edit_novo)
 
         # Botão copiar
-        self.btn_copiar = QPushButton(text="Copiar")
+        self.btn_copiar = QtWidgets.QPushButton(text="Copiar")
         self.layout.addWidget(self.btn_copiar)
 
         # Sinais
         self.btn_copiar.clicked.connect(self.copia_caminho)
         self.edit_original.textChanged.connect(self.atualiza_caminho)
 
+        # Se houver sido passado um local por argv, atualiza.
         if local:
             self.edit_original.setText(local)
             self.edit_novo.setText(self.formata_local(local))
@@ -72,7 +63,7 @@ class JanelaPrincipal(QMainWindow):
     def copia_caminho(self) -> None:
         self.clipboard.setText(self.edit_novo.text())
 
-    def dropou_na_area(self, mime_data: QMimeData) -> None:
+    def dropou_na_area(self, mime_data: QtCore.QMimeData) -> None:
         if mime_data.hasUrls():
             self.edit_original.setText(mime_data.urls()[0].path())
         else:
@@ -89,14 +80,13 @@ class JanelaPrincipal(QMainWindow):
                     caminho_macos = "smb://fileserver/" + caminho_limpo
                     return caminho_macos.replace(" ", "%20")
         except ValueError:
-            pass
-        return "Caminho inválido!"
+            return "Caminho inválido!"
 
 
 def main() -> None:
     """Início do programa"""
 
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     app.setStyle("Fusion")
 
     try:
@@ -109,7 +99,7 @@ def main() -> None:
     janela = JanelaPrincipal(local=local, clipboard=app.clipboard())
     janela.show()
 
-    app.exec()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
